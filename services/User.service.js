0ca4acf7840ed05354ad1bc6060a2c92abc94a27
rodrigo <rbp20199@gmail.com>
2022-10-26 13:50:30 -0300

    bug login fixed

--- services/User.service.js
+++ services/User.service.js
@@ -39,7 +39,7 @@ exports.login = async (email, password) => {
 
     const user = await this.findUserByEmail(email)
 
-    if (!user.response) {
+    if (!user.status || user.status == 500) {
         return {
             status: 401,
             msg: 'usuário ou senha inválidos',
